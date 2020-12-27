49117eac48a20ad944667d69991783de0b5f7863
Shuai Wang <ws1011001@gmail.com>
2020-12-27 14:31:21 +0100

    edited to run.

--- slurm_afni.sh
+++ slurm_afni.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 
-#SBATCH -J RETROICOR
+#SBATCH -J AFNI-GLM
 #SBATCH -p skylake
 #SBATCH --nodes=1
 #SBATCH -A a222
 #SBATCH -t 1-12
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=128gb
-#SBATCH -o ./RETROICOR_%j.out
-#SBATCH -e ./RETROICOR_%j.err
+#SBATCH -o ./AFNI_%j.out
+#SBATCH -e ./AFNI_%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ws1011001@gmail.com
 #SBATCH --mail-type=BEGIN,END
 
 # setup path
-idir='/scratch/swang/simages'  # singularity images directory
-mdir='/scratch/swang/agora/CP00'     # the project main directory
-scripts='/CP00/scripts'        # scripts folder in Sy
+idir='/scratch/swang/simages'     # singularity images directory
+mdir='/scratch/swang/agora/CP00'  # the project main directory
+scripts='/CP00/scripts'           # scripts folder in Sy
 
 # processing log
-#echo -e 'Running ps00_DATA_stimuli_timings.sh with singularity'
-#singularity exec --bind $mdir:/CP00 $idir/nidebian-1.1.2 bash $scripts/ps00_DATA_stimuli_timings.sh
-
-#echo -e 'Running ps01_PREPROC_ricor_afni.sh with singularity'
-#singularity exec --bind $mdir:/CP00 $idir/nidebian-1.1.2 bash $scripts/ps01_PREPROC_ricor_afni.sh
-
+# 2020-12-27T14:30
 echo -e 'Running ps02_GLM_LocaVis1p75_wNR50_afni.sh with singularity'
 singularity exec --bind $mdir:/CP00 $idir/nidebian-1.1.2 bash $scripts/ps02_GLM_LocaVis1p75_wNR50_afni.sh
