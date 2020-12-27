954d7d3f11297cfe7870d34f82153b72d6130b08
Shuai Wang <ws1011001@gmail.com>
2020-12-27 14:25:01 +0100

    edited to run.

--- ps02_GLM_LocaVis1p75_wNR50_afni.sh
+++ ps02_GLM_LocaVis1p75_wNR50_afni.sh
@@ -34,7 +34,7 @@ esac
 ddir="$mdir/AudioVisAsso"                # experiment Data folder (BIDS put into fMRIPrep)
 adir="$ddir/derivatives/afni"            # AFNI output folder
 # processing parameters
-readarray subjects < $mdir/CP00_subjects_RF.txt
+readarray subjects < $mdir/CP00_subjects.txt
 task='task-LocaVis1p75'           # task name
 spac='space-MNI152NLin2009cAsym'  # anatomical template that used for preprocessing by fMRIPrep
 bold='desc-preproc_bold'          # the token for the preprocessed BOLD data (without smoothing)
@@ -88,3 +88,9 @@ for subj in ${subjects[@]};do
   tar -cvzf $wdir/confounds/${oglm}.1D.tar.gz $wdir/confounds/*.1D
   rm -r $wdir/confounds/*.1D
 done
+## ---------------------------
+
+## summarize data quality metrics
+gen_ss_review_table.py -write_table $adir/review_QC_${task}_GLM.w${deno}.tsv \
+  -infiles $adir/sub-*/$task/sub-*_${task}_GLM.w${deno}/out.ss_review.sub-*_${task}.txt -overwrite
+## ---------------------------
