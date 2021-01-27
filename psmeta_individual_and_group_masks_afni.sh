df567e4eec6c5b4f7ae9f701c03acf9b5908a20b
Shuai Wang <ws1011001@gmail.com>
2021-01-27 01:17:10 +0100

    edited script to create better masks.

--- psmeta_individual_and_group_masks_afni.sh
+++ psmeta_individual_and_group_masks_afni.sh
@@ -42,12 +42,12 @@ bold='desc-preproc_bold'          # the token for the preprocessed BOLD data (wi
 regs='desc-confounds_timeseries'  # the token for fMRIPrep output nuisance regressors
 anat='desc-preproc_T1w_brain'     # skull-stripped anatomical image
 deno='NR14'                       # denoising strategy
-gmth=0.2                          # gray matter threshold
+gmth=0.2                          # gray matter threshold between [0 1]
 ## ---------------------------
 
 ## create masks for each subject
+echo -e "========== start creating masks at $(date) =========="
 for subj in ${subjects[@]};do
-  echo -e "========== start creating masks for subject : $subj at $(date) =========="
 
   gdir="$pdir/$subj/anat"                   # individual folder that contains anatomical segments
   wdir="$adir/$subj/$task"                  # the Working folder
@@ -56,20 +56,24 @@ for subj in ${subjects[@]};do
   if [ ! -d $sdir ];then mkdir -p $sdir;fi
 
   # prepare files to create masks
-  gm_seg="$gdir/${subj}_${spac}_label-GM_probseg.nii.gz"              # gray matter probability segment
-  gm_mask0="$sdir/${subj}_${spac}_GM${gmth}_mask_res-anat.nii.gz"     # gray matter mask in T1 resolution
-  gm_mask1="$sdir/${subj}_${spac}_GM${gmth}_mask_res-${task}.nii.gz"  # gray matter mask in task resolution
-  epi_mask="$wdir/$oglm/mask_epi_anat.${subj}_${task}+tlrc."          # EPI extent mask
-  gm_epi_mask="$sdir/${subj}_${spac}_GM+EPI_mask_res-${task}.nii.gz"  # EPI constrained GM mask
-
+  gm_segment="$gdir/${subj}_${spac}_label-GM_probseg.nii.gz"            # gray matter segment in probability 
+  gm_mask_t1="$sdir/${subj}_${spac}_mask-gm${gmth}_res-anat.nii.gz"     # gray matter mask in T1 resolution
+  gm_mask_bd="$sdir/${subj}_${spac}_mask-gm${gmth}_res-${task}.nii.gz"  # gray matter mask in task resolution
+  stats_afni="$wdir/$oglm/stats.${subj}_${task}+tlrc.[0]"               # AFNI statistics
+  stats_mask="$sdir/${subj}_${spac}_mask-full-F_res-${task}.nii.gz"     # functional mask based on full F statistics
+  gm_fF_mask="$sdir/${subj}_${spac}_mask-gm-full-F_res-${task}.nii.gz"  # gray matter mask constrained by full F mask
+  # prepare individual files for creating group masks
+  cp -r $gm_segment $sdir
+  # calculate functional mask using full F statistics
+  3dcalc -a $stats_afni -expr 'ispositive(a)' -prefix $stats_mask
   # create individual gray matter masks
-  3dcalc -a $gm_seg -expr "ispositive(a-$gmth)" -prefix $gm_mask0
-  3dresample -master $epi_mask -prefix $gm_mask1 -input $gm_mask0
-  # create EPI-constrained gray matter masks
-  3dcalc -a $gm_mask1 -b $epi_mask -expr 'a*b' -prefix $gm_epi_mask
+  3dcalc -a $gm_segment -expr "ispositive(a-$gmth)" -prefix $gm_mask_t1
+  3dresample -master $stats_mask -prefix $gm_mask_bd -input $gm_mask_t1
+  # create functionally constrained gray matter masks
+  3dcalc -a $gm_mask_bd -b $stats_mask -expr 'a*b' -prefix $gm_fF_mask
 
-  echo -e "========== finish creating masks for subject : $subj at $(date) =========="
 done
+echo -e "========== finish creating masks at $(date) =========="
 ## ---------------------------
 
 ## create group-averaged masks
