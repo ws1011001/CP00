3de36b320713461d9ab1a42c00eddf3181471d41
Shuai Wang <ws1011001@gmail.com>
2021-02-09 11:50:02 +0100

    cached.

--- ps13_LSS_AudioVisAssos1word_wPSC_wNR24a_afni.sh
+++ ps13_LSS_AudioVisAssos1word_wPSC_wNR24a_afni.sh
@@ -55,10 +55,10 @@ cons=("WA" "WV" "PA" "PV")            # conditions
 for subj in ${subjects[@]};do
   echo -e "run LSS for $task for subject : $subj ......"
 
-  wdir="$adir/$subj/$task"                              # the Working folder
-  oglm="${subj}_${task}_GLM.wBIM.wPSC.w${deno}"         # the token for the Output GLM, psc means "percent signal change"
-  ldir="$wdir/$oglm/trial-wise_estimates"               # the LSS working folder
-  mask="$wdir/${subj}_${task}_${spac}_desc-brain_mask"  # brain mask that used for 3dLSS
+  wdir="$adir/$subj/$task"                                     # the Working folder
+  oglm="${subj}_${task}_GLM.wBIM.wPSC.w${deno}"                # the token for the Output GLM, psc means "percent signal change"
+  ldir="$wdir/$oglm/trial-wise_estimates"                      # the LSS working folder
+  mask="$wdir/${subj}_${task}_${spac}_desc-brain_mask.nii.gz"  # brain mask that used for 3dLSS
   
   # create a working folder for LSS
   if [ ! -d $ldir ];then
@@ -68,124 +68,124 @@ for subj in ${subjects[@]};do
   echo -e "# enter the LSS working directory..."
   cd $ldir  # enter the LSS working directory
 
-  # concatenate all scaled runs
-  3dTcat -prefix LSS.${subj}_${task}.all.scale $wdir/$oglm/pb02.${subj}_${task}.r*.scale+tlrc.HEAD  
+#  # concatenate all scaled runs
+#  3dTcat -prefix LSS.${subj}_${task}.all.scale $wdir/$oglm/pb02.${subj}_${task}.r*.scale+tlrc.HEAD  
+#  
+#  # prepare nuisance regressors
+#  tar -mvxf $wdir/confounds/${oglm}.1D.tar.gz --strip-components 7 -C $wdir/confounds  # use --strip-components 7 to remove folder structure (a bad but only solution)
+#  cp -r $wdir/confounds/${subj}_${task}_${cenv}.1D $ldir
+#  1d_tool.py -infile $wdir/confounds/${subj}_${task}_${hmpv}.1D -set_run_lengths ${lrun[*]} -demean -write ${subj}_${task}_${hmpv}_demean.1D
+#  1d_tool.py -infile ${subj}_${task}_${hmpv}_demean.1D -set_run_lengths ${lrun[*]} -split_into_pad_runs ${subj}_${task}_${hmpv}_demean_all
+#  for irun in $(seq 1 $nrun);do
+#    frun=`printf "%02d" $irun`
+#    1d_tool.py -infile $wdir/confounds/${subj}_${task}_run-${frun}_${ortv}.1D -demean -write ${subj}_${task}_run-${frun}_${ortv}_demean.1D
+#    1d_tool.py -infile ${subj}_${task}_run-${frun}_${ortv}_demean.1D -pad_into_many_runs $irun $nrun -set_run_lengths ${lrun[*]} -write ${subj}_${task}_run-${frun}_${ortv}_demean_all.1D
+#  done
+#  rm -r $wdir/confounds/*.1D
+#  
+#  # generate the global timing
+#  for icond in stimuli/${subj}_${task}_events-cond?.txt;do
+#    timing_tool.py -timing $icond -local_to_global ${icond::-4}_global.txt -run_len ${drun[*]}
+#  done
   
-  # prepare nuisance regressors
-  tar -mvxf $wdir/confounds/${oglm}.1D.tar.gz --strip-components 7 -C $wdir/confounds  # use --strip-components 7 to remove folder structure (a bad but only solution)
-  cp -r $wdir/confounds/${subj}_${task}_${cenv}.1D $ldir
-  1d_tool.py -infile $wdir/confounds/${subj}_${task}_${hmpv}.1D -set_run_lengths ${lrun[*]} -demean -write ${subj}_${task}_${hmpv}_demean.1D
-  1d_tool.py -infile ${subj}_${task}_${hmpv}_demean.1D -set_run_lengths ${lrun[*]} -split_into_pad_runs ${subj}_${task}_${hmpv}_demean_all
-  for irun in $(seq 1 $nrun);do
-    frun=`printf "%02d" $irun`
-    1d_tool.py -infile $wdir/confounds/${subj}_${task}_run-${frun}_${ortv}.1D -demean -write ${subj}_${task}_run-${frun}_${ortv}_demean.1D
-    1d_tool.py -infile ${subj}_${task}_run-${frun}_${ortv}_demean.1D -pad_into_many_runs $irun $nrun -set_run_lengths ${lrun[*]} -write ${subj}_${task}_run-${frun}_${ortv}_demean_all.1D
-  done
-  rm -r $wdir/confounds/*.1D
-  
-  # generate the global timing
-  for icond in stimuli/${subj}_${task}_events-cond?.txt;do
-    timing_tool.py -timing $icond -local_to_global ${icond::-4}_global.txt -run_len ${drun[*]}
-  done
-  
-  # create the design matrix for each condition
-  # WA
-  cprefix='con1_WA'
-  3dDeconvolve -nodata $ntp $tr \
-      -censor ${subj}_${task}_${cenv}.1D \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r01.1D head_motion_run1 \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r02.1D head_motion_run2 \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r03.1D head_motion_run3 \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r04.1D head_motion_run4 \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r05.1D head_motion_run5 \
-      -ortvec ${subj}_${task}_run-01_${ortv}_demean_all.1D nuisance_regressors_run1 \
-      -ortvec ${subj}_${task}_run-02_${ortv}_demean_all.1D nuisance_regressors_run2 \
-      -ortvec ${subj}_${task}_run-03_${ortv}_demean_all.1D nuisance_regressors_run3 \
-      -ortvec ${subj}_${task}_run-04_${ortv}_demean_all.1D nuisance_regressors_run4 \
-      -ortvec ${subj}_${task}_run-05_${ortv}_demean_all.1D nuisance_regressors_run5 \
-      -polort 2 \
-      -global_times \
-      -num_stimts 4 \
-      -stim_times_IM 1 stimuli/${subj}_${task}_events-cond1_global.txt GAM -stim_label 1 WA \
-      -stim_times 2 stimuli/${subj}_${task}_events-cond2_global.txt GAM -stim_label 2 WV \
-      -stim_times 3 stimuli/${subj}_${task}_events-cond3_global.txt GAM -stim_label 3 PA \
-      -stim_times 4 stimuli/${subj}_${task}_events-cond4_global.txt GAM -stim_label 4 PV \
-      -jobs $njob \
-      -x1D ${cprefix}_X.xmat.1D -xjpeg ${cprefix}_X.jpg -x1D_uncensored ${cprefix}_X.nocensor.xmat.1D \
-      -x1D_stop
-  # WV
-  cprefix='con2_WV'
-  3dDeconvolve -nodata $ntp $tr \
-      -censor ${subj}_${task}_${cenv}.1D \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r01.1D head_motion_run1 \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r02.1D head_motion_run2 \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r03.1D head_motion_run3 \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r04.1D head_motion_run4 \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r05.1D head_motion_run5 \
-      -ortvec ${subj}_${task}_run-01_${ortv}_demean_all.1D nuisance_regressors_run1 \
-      -ortvec ${subj}_${task}_run-02_${ortv}_demean_all.1D nuisance_regressors_run2 \
-      -ortvec ${subj}_${task}_run-03_${ortv}_demean_all.1D nuisance_regressors_run3 \
-      -ortvec ${subj}_${task}_run-04_${ortv}_demean_all.1D nuisance_regressors_run4 \
-      -ortvec ${subj}_${task}_run-05_${ortv}_demean_all.1D nuisance_regressors_run5 \
-      -polort 2 \
-      -global_times \
-      -num_stimts 4 \
-      -stim_times 1 stimuli/${subj}_${task}_events-cond1_global.txt GAM -stim_label 1 WA \
-      -stim_times_IM 2 stimuli/${subj}_${task}_events-cond2_global.txt GAM -stim_label 2 WV \
-      -stim_times 3 stimuli/${subj}_${task}_events-cond3_global.txt GAM -stim_label 3 PA \
-      -stim_times 4 stimuli/${subj}_${task}_events-cond4_global.txt GAM -stim_label 4 PV \
-      -jobs $njob \
-      -x1D ${cprefix}_X.xmat.1D -xjpeg ${cprefix}_X.jpg -x1D_uncensored ${cprefix}_X.nocensor.xmat.1D \
-      -x1D_stop
-  # PA
-  cprefix='con3_PA'
-  3dDeconvolve -nodata $ntp $tr \
-      -censor ${subj}_${task}_${cenv}.1D \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r01.1D head_motion_run1 \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r02.1D head_motion_run2 \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r03.1D head_motion_run3 \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r04.1D head_motion_run4 \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r05.1D head_motion_run5 \
-      -ortvec ${subj}_${task}_run-01_${ortv}_demean_all.1D nuisance_regressors_run1 \
-      -ortvec ${subj}_${task}_run-02_${ortv}_demean_all.1D nuisance_regressors_run2 \
-      -ortvec ${subj}_${task}_run-03_${ortv}_demean_all.1D nuisance_regressors_run3 \
-      -ortvec ${subj}_${task}_run-04_${ortv}_demean_all.1D nuisance_regressors_run4 \
-      -ortvec ${subj}_${task}_run-05_${ortv}_demean_all.1D nuisance_regressors_run5 \
-      -polort 2 \
-      -global_times \
-      -num_stimts 4 \
-      -stim_times 1 stimuli/${subj}_${task}_events-cond1_global.txt GAM -stim_label 1 WA \
-      -stim_times 2 stimuli/${subj}_${task}_events-cond2_global.txt GAM -stim_label 2 WV \
-      -stim_times_IM 3 stimuli/${subj}_${task}_events-cond3_global.txt GAM -stim_label 3 PA \
-      -stim_times 4 stimuli/${subj}_${task}_events-cond4_global.txt GAM -stim_label 4 PV \
-      -jobs $njob \
-      -x1D ${cprefix}_X.xmat.1D -xjpeg ${cprefix}_X.jpg -x1D_uncensored ${cprefix}_X.nocensor.xmat.1D \
-      -x1D_stop
-  # PV
-  cprefix='con4_PV'
-  3dDeconvolve -nodata $ntp $tr \
-      -censor ${subj}_${task}_${cenv}.1D \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r01.1D head_motion_run1 \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r02.1D head_motion_run2 \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r03.1D head_motion_run3 \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r04.1D head_motion_run4 \
-      -ortvec ${subj}_${task}_${hmpv}_demean_all.r05.1D head_motion_run5 \
-      -ortvec ${subj}_${task}_run-01_${ortv}_demean_all.1D nuisance_regressors_run1 \
-      -ortvec ${subj}_${task}_run-02_${ortv}_demean_all.1D nuisance_regressors_run2 \
-      -ortvec ${subj}_${task}_run-03_${ortv}_demean_all.1D nuisance_regressors_run3 \
-      -ortvec ${subj}_${task}_run-04_${ortv}_demean_all.1D nuisance_regressors_run4 \
-      -ortvec ${subj}_${task}_run-05_${ortv}_demean_all.1D nuisance_regressors_run5 \
-      -polort 2 \
-      -global_times \
-      -num_stimts 4 \
-      -stim_times 1 stimuli/${subj}_${task}_events-cond1_global.txt GAM -stim_label 1 WA \
-      -stim_times 2 stimuli/${subj}_${task}_events-cond2_global.txt GAM -stim_label 2 WV \
-      -stim_times 3 stimuli/${subj}_${task}_events-cond3_global.txt GAM -stim_label 3 PA \
-      -stim_times_IM 4 stimuli/${subj}_${task}_events-cond4_global.txt GAM -stim_label 4 PV \
-      -jobs $njob \
-      -x1D ${cprefix}_X.xmat.1D -xjpeg ${cprefix}_X.jpg -x1D_uncensored ${cprefix}_X.nocensor.xmat.1D \
-      -x1D_stop
-  
+#  # create the design matrix for each condition
+#  # WA
+#  cprefix='con1_WA'
+#  3dDeconvolve -nodata $ntp $tr \
+#      -censor ${subj}_${task}_${cenv}.1D \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r01.1D head_motion_run1 \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r02.1D head_motion_run2 \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r03.1D head_motion_run3 \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r04.1D head_motion_run4 \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r05.1D head_motion_run5 \
+#      -ortvec ${subj}_${task}_run-01_${ortv}_demean_all.1D nuisance_regressors_run1 \
+#      -ortvec ${subj}_${task}_run-02_${ortv}_demean_all.1D nuisance_regressors_run2 \
+#      -ortvec ${subj}_${task}_run-03_${ortv}_demean_all.1D nuisance_regressors_run3 \
+#      -ortvec ${subj}_${task}_run-04_${ortv}_demean_all.1D nuisance_regressors_run4 \
+#      -ortvec ${subj}_${task}_run-05_${ortv}_demean_all.1D nuisance_regressors_run5 \
+#      -polort 2 \
+#      -global_times \
+#      -num_stimts 4 \
+#      -stim_times_IM 1 stimuli/${subj}_${task}_events-cond1_global.txt GAM -stim_label 1 WA \
+#      -stim_times 2 stimuli/${subj}_${task}_events-cond2_global.txt GAM -stim_label 2 WV \
+#      -stim_times 3 stimuli/${subj}_${task}_events-cond3_global.txt GAM -stim_label 3 PA \
+#      -stim_times 4 stimuli/${subj}_${task}_events-cond4_global.txt GAM -stim_label 4 PV \
+#      -jobs $njob \
+#      -x1D ${cprefix}_X.xmat.1D -xjpeg ${cprefix}_X.jpg -x1D_uncensored ${cprefix}_X.nocensor.xmat.1D \
+#      -x1D_stop
+#  # WV
+#  cprefix='con2_WV'
+#  3dDeconvolve -nodata $ntp $tr \
+#      -censor ${subj}_${task}_${cenv}.1D \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r01.1D head_motion_run1 \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r02.1D head_motion_run2 \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r03.1D head_motion_run3 \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r04.1D head_motion_run4 \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r05.1D head_motion_run5 \
+#      -ortvec ${subj}_${task}_run-01_${ortv}_demean_all.1D nuisance_regressors_run1 \
+#      -ortvec ${subj}_${task}_run-02_${ortv}_demean_all.1D nuisance_regressors_run2 \
+#      -ortvec ${subj}_${task}_run-03_${ortv}_demean_all.1D nuisance_regressors_run3 \
+#      -ortvec ${subj}_${task}_run-04_${ortv}_demean_all.1D nuisance_regressors_run4 \
+#      -ortvec ${subj}_${task}_run-05_${ortv}_demean_all.1D nuisance_regressors_run5 \
+#      -polort 2 \
+#      -global_times \
+#      -num_stimts 4 \
+#      -stim_times 1 stimuli/${subj}_${task}_events-cond1_global.txt GAM -stim_label 1 WA \
+#      -stim_times_IM 2 stimuli/${subj}_${task}_events-cond2_global.txt GAM -stim_label 2 WV \
+#      -stim_times 3 stimuli/${subj}_${task}_events-cond3_global.txt GAM -stim_label 3 PA \
+#      -stim_times 4 stimuli/${subj}_${task}_events-cond4_global.txt GAM -stim_label 4 PV \
+#      -jobs $njob \
+#      -x1D ${cprefix}_X.xmat.1D -xjpeg ${cprefix}_X.jpg -x1D_uncensored ${cprefix}_X.nocensor.xmat.1D \
+#      -x1D_stop
+#  # PA
+#  cprefix='con3_PA'
+#  3dDeconvolve -nodata $ntp $tr \
+#      -censor ${subj}_${task}_${cenv}.1D \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r01.1D head_motion_run1 \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r02.1D head_motion_run2 \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r03.1D head_motion_run3 \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r04.1D head_motion_run4 \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r05.1D head_motion_run5 \
+#      -ortvec ${subj}_${task}_run-01_${ortv}_demean_all.1D nuisance_regressors_run1 \
+#      -ortvec ${subj}_${task}_run-02_${ortv}_demean_all.1D nuisance_regressors_run2 \
+#      -ortvec ${subj}_${task}_run-03_${ortv}_demean_all.1D nuisance_regressors_run3 \
+#      -ortvec ${subj}_${task}_run-04_${ortv}_demean_all.1D nuisance_regressors_run4 \
+#      -ortvec ${subj}_${task}_run-05_${ortv}_demean_all.1D nuisance_regressors_run5 \
+#      -polort 2 \
+#      -global_times \
+#      -num_stimts 4 \
+#      -stim_times 1 stimuli/${subj}_${task}_events-cond1_global.txt GAM -stim_label 1 WA \
+#      -stim_times 2 stimuli/${subj}_${task}_events-cond2_global.txt GAM -stim_label 2 WV \
+#      -stim_times_IM 3 stimuli/${subj}_${task}_events-cond3_global.txt GAM -stim_label 3 PA \
+#      -stim_times 4 stimuli/${subj}_${task}_events-cond4_global.txt GAM -stim_label 4 PV \
+#      -jobs $njob \
+#      -x1D ${cprefix}_X.xmat.1D -xjpeg ${cprefix}_X.jpg -x1D_uncensored ${cprefix}_X.nocensor.xmat.1D \
+#      -x1D_stop
+#  # PV
+#  cprefix='con4_PV'
+#  3dDeconvolve -nodata $ntp $tr \
+#      -censor ${subj}_${task}_${cenv}.1D \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r01.1D head_motion_run1 \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r02.1D head_motion_run2 \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r03.1D head_motion_run3 \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r04.1D head_motion_run4 \
+#      -ortvec ${subj}_${task}_${hmpv}_demean_all.r05.1D head_motion_run5 \
+#      -ortvec ${subj}_${task}_run-01_${ortv}_demean_all.1D nuisance_regressors_run1 \
+#      -ortvec ${subj}_${task}_run-02_${ortv}_demean_all.1D nuisance_regressors_run2 \
+#      -ortvec ${subj}_${task}_run-03_${ortv}_demean_all.1D nuisance_regressors_run3 \
+#      -ortvec ${subj}_${task}_run-04_${ortv}_demean_all.1D nuisance_regressors_run4 \
+#      -ortvec ${subj}_${task}_run-05_${ortv}_demean_all.1D nuisance_regressors_run5 \
+#      -polort 2 \
+#      -global_times \
+#      -num_stimts 4 \
+#      -stim_times 1 stimuli/${subj}_${task}_events-cond1_global.txt GAM -stim_label 1 WA \
+#      -stim_times 2 stimuli/${subj}_${task}_events-cond2_global.txt GAM -stim_label 2 WV \
+#      -stim_times 3 stimuli/${subj}_${task}_events-cond3_global.txt GAM -stim_label 3 PA \
+#      -stim_times_IM 4 stimuli/${subj}_${task}_events-cond4_global.txt GAM -stim_label 4 PV \
+#      -jobs $njob \
+#      -x1D ${cprefix}_X.xmat.1D -xjpeg ${cprefix}_X.jpg -x1D_uncensored ${cprefix}_X.nocensor.xmat.1D \
+#      -x1D_stop
+#  
   # run 3dLSS to obtain trial-wise estimates
   i=1
   for icon in ${cons[@]};do
