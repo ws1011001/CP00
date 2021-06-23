b345040a706e5de9500f71dba074b332048b6e3b
Shuai Wang <ws1011001@gmail.com>
2021-06-24 01:33:42 +0200

    cached.

--- slurm_nilearn.sh
+++ slurm_nilearn.sh
@@ -5,8 +5,8 @@
 #SBATCH --nodes=1
 #SBATCH -A b222
 #SBATCH -t 1-12
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=160gb
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=128gb
 #SBATCH -o ./run_slurm.log/nilearn_%j.out
 #SBATCH -e ./run_slurm.log/nilearn_%j.err
 #SBATCH --mail-type=ALL
@@ -24,7 +24,7 @@ ni_log='run_nilearn.log'  # records of running nilearn scripts with slurm
 SECONDS=0
 echo -e "========== Start running $script_run with slurm at $(date) =========="
 echo -e "$(date) : $script_run" >> $ni_log
-#conda run --name base python $script_run
+conda run --name base python $script_run
 echo -e "========== Finish $script_run with slurm at $(date) =========="
 duration=$SECONDS
 echo -e "$(date) : $script_run - $(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed." >> $ni_log
