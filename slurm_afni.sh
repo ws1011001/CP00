255fb48826f3778b18d141837ffdc3dceb017db6
Shuai Wang <ws1011001@gmail.com>
2021-09-21 11:13:46 +0200

    cached.

--- slurm_afni.sh
+++ slurm_afni.sh
@@ -36,7 +36,7 @@ scripts_afni[13]='ps17_STAT_AudioVisAssos1word_factorial_afni.sh'
 scripts_afni[14]='ps22_STAT_AudioVisAssos1word_searchlight_maps_afni.sh'
 scripts_afni[15]='ps26_STAT_AudioVisAssos1word_searchlight_RSA_afni.sh'
 scripts_afni[16]='psmeta_individual_and_group_masks_afni.sh'
-script_id=15
+script_id=16
 
 # run AFNI script
 script_run=${scripts_afni[$script_id]}
