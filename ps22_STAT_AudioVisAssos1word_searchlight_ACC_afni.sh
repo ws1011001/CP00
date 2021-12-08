85207c897dda5eb7d4f385745ef4480b982598ac
Shuai Wang <ws1011001@gmail.com>
2021-12-08 14:34:27 +0100

    cached.

--- ps22_STAT_AudioVisAssos1word_searchlight_ACC_afni.sh
+++ ps22_STAT_AudioVisAssos1word_searchlight_ACC_afni.sh
@@ -62,13 +62,13 @@ for roi in ${rois[@]};do
 #    done
     # stack up subjects for group analysis
     for imod in ${mods[@]};do
-      facc="$gdir/stats.acc_group_${mvpc}-${clf}_LOROCV_ACC-${imod}_mask-${roi}.nii.gz"
+#      facc="$gdir/stats.acc_group_${mvpc}-${clf}_LOROCV_ACC-${imod}_mask-${roi}.nii.gz"
 #      3dbucket -fbuc -aglueto $facc $vdir/sub-*/$mvpc/sub-*_${mvpc}-${clf}_LOROCV_ACC-${imod}_mask-${roi}.nii.gz
       # calculate above-chance ACC
       fabv="$gdir/stats.acc_group_${mvpc}-${clf}_LOROCV_ACC-above-chance-${imod}_mask-${roi}.nii.gz"
-      3dcalc -a $facc -expr "a-$base_acc" -prefix $fabv
+#      3dcalc -a $facc -expr "a-$base_acc" -prefix $fabv
       # T-test on one sample againest the chance level
-      3dttest++ -setA $fabv -mask $mask -exblur 6 -prefix $gdir/stats.group_${mvpc}-${clf}_LOROCV_ACC-above-chance-${imod}_mask-${roi}.nii.gz
+      3dttest++ -setA $fabv -mask $mask -exblur 4 -prefix $gdir/stats.group_${mvpc}-${clf}_LOROCV_ACC-above-chance-${imod}_mask-${roi}.nii.gz
 #      3dttest++ -singletonA $base_acc -setB $facc -mask $mask -exblur 6 -prefix $gdir/stats.group_${mvpc}-${clf}_LOROCV_ACC-${imod}_mask-${roi}_blur-6mm.nii.gz  # -singletonA doesn't work well with -exblur
     done
   done
