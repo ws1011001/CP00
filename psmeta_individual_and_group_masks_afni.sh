41f738286d03164432a5a756238a9218813692b1
Shuai Wang <ws1011001@gmail.com>
2022-03-31 11:08:42 +0200

    Created spherical ROIs using individual coordinates.

--- psmeta_individual_and_group_masks_afni.sh
+++ psmeta_individual_and_group_masks_afni.sh
@@ -139,22 +139,36 @@ fi
 if $isCreateCoord;then
   cdir="$kdir/coordinates"
   fcrd="$cdir/group_${spac}_mask-coordinates.csv"
+  filv="$cdir/group_${spac}_mask-ilvOT-coordinates.csv"
   fmas="$kdir/group/group_${spac}_mask-lvOT-visual.nii.gz"
   rads=(7 8)  # 4,5,6,7,8
   # create mask for each coordinate
   OLDIFS=$IFS  # original delimiter
   IFS=','      # delimiter of CSV
-  while read thisroi x y z;do
+#  # group ROIs
+#  while read thisroi x y z;do
+#    for srad in ${rads[@]};do
+#      froi="$cdir/group_${spac}_mask-${thisroi}-sph${srad}mm.nii.gz"
+#      if [ ! -f "$froi" ];then
+#        echo -e "Create a shpere ROI $thisroi with radius ${srad}mm and centre $x $y $z."
+#        echo "$x $y $z 1" > $cdir/${thisroi}.peak
+#        3dUndump -master $fmas -srad $srad -prefix $froi -xyz $cdir/${thisroi}.peak
+#        rm -r $cdir/${thisroi}.peak
+#      fi  
+#    done
+#  done < $fcrd
+  # individual left-vOT (ilvOT)
+  while read subj x y z;do
     for srad in ${rads[@]};do
-      froi="$cdir/group_${spac}_mask-${thisroi}-sph${srad}mm.nii.gz"
+      froi="$kdir/$subj/${subj}_${spac}_mask-ilvOT-sph${srad}mm.nii.gz"
       if [ ! -f "$froi" ];then
-        echo -e "Create a shpere ROI $thisroi with radius ${srad}mm and centre $x $y $z."
-        echo "$x $y $z 1" > $cdir/${thisroi}.peak
-        3dUndump -master $fmas -srad $srad -prefix $froi -xyz $cdir/${thisroi}.peak
-        rm -r $cdir/${thisroi}.peak
+        echo -e "Create a shpere ROI ilvOT with radius ${srad}mm and centre $x $y $z."
+        echo "$x $y $z 1" > $cdir/${subj}_ilvOT.peak
+        3dUndump -master $fmas -srad $srad -prefix $froi -xyz $cdir/${subj}_ilvOT.peak
+        rm -r $cdir/${subj}_ilvOT.peak
       fi  
     done
-  done < $fcrd
+  done < $filv
   IFS=$OLDIFS
 fi
 ## ---------------------------
