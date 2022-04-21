8a919b6c6baffea0f74b3c3b981839ac075627d6
Shuai Wang <ws1011001@gmail.com>
2022-04-21 12:52:29 +0200

    Added codes to extract PSC with different ROIs.

--- ps17_STAT_AudioVisAssos1word_factorial_afni.sh
+++ ps17_STAT_AudioVisAssos1word_factorial_afni.sh
@@ -35,10 +35,14 @@ adir="$ddir/derivatives/afni"   # AFNI output folder
 kdir="$ddir/derivatives/masks"  # masks folder
 # processing parameters
 readarray subjects < $mdir/CP00_subjects.txt
-task='AudioVisAssos1word'         # task name
+readarray rois < $adir/group_masks_labels-AVA.txt
+task='task-AudioVisAssos1word'    # task name
 spac='space-MNI152NLin2009cAsym'  # anatomical template that used for preprocessing by fMRIPrep
-mask="$kdir/group/group_${spac}_mask-gm-final_res-${task}.nii.gz"  # GM mask
+mask="$kdir/group/group_${spac}_mask-gm-0.2_res-${task}.nii.gz"  # GM mask
 models=("GLM.wBIM.wPSC.wNR24a")
+# index the stat volumes
+eidx=(1 4 7 10)
+flab=("WA" "WV" "PA" "PV")
 ## ---------------------------
 
 echo -e "========== START JOB at $(date) =========="
@@ -47,32 +51,85 @@ echo -e "========== START JOB at $(date) =========="
 tdir="$adir/group/$task"
 if [ ! -d $tdir ];then mkdir -p $tdir;fi
 for model in ${models[@]};do
-  # generate the datatable for 3dMVM
   ftab="$tdir/stats.group_${task}_${model}_MVM.dataTable"
+  # generate the datatable for 3dMVM
   if [ ! -f "$ftab" ];then
     echo "Subj modality lexicon InputFile" >> $ftab
     for subj in ${subjects[@]};do 
-      sdir="$adir/$subj/task-${task}/${subj}_task-${task}_${model}"
-      echo "$subj auditory word $sdir/stats.${subj}_task-${task}+tlrc.[WA#0_Coef]" >> $ftab
-      echo "$subj auditory pseudoword $sdir/stats.${subj}_task-${task}+tlrc.[PA#0_Coef]" >> $ftab
-      echo "$subj visual word $sdir/stats.${subj}_task-${task}+tlrc.[WV#0_Coef]" >> $ftab
-      echo "$subj visual pseudoword $sdir/stats.${subj}_task-${task}+tlrc.[PV#0_Coef]" >> $ftab
+      sdir="$adir/$subj/${task}/${subj}_${task}_${model}"
+      echo "$subj auditory word $sdir/stats.${subj}_${task}+tlrc.[WA#0_Coef]" >> $ftab
+      echo "$subj auditory pseudoword $sdir/stats.${subj}_${task}+tlrc.[PA#0_Coef]" >> $ftab
+      echo "$subj visual word $sdir/stats.${subj}_${task}+tlrc.[WV#0_Coef]" >> $ftab
+      echo "$subj visual pseudoword $sdir/stats.${subj}_${task}+tlrc.[PV#0_Coef]" >> $ftab
     done
   fi
   # perform MVM
-  3dMVM -prefix $tdir/stats.group_${task}_${model}_MVM \
-    -jobs $njob \
-    -bsVars 1 \
-    -wsVars "modality*lexicon" \
-    -SS_type 3 \
-    -num_glt 6 \
-    -gltLabel 1 auditory_vs_visual -gltCode 1 'modality : 1*auditory -1*visual' \
-    -gltLabel 2 auditory_vs_visual_word -gltCode 2 'modality : 1*auditory -1*visual lexicon : 1*word' \
-    -gltLabel 3 auditory_vs_visual_pseudoword -gltCode 3 'modality : 1*auditory -1*visual lexicon : 1*pseudoword' \
-    -gltLabel 4 word_vs_pseudoword -gltCode 4 'lexicon : 1*word -1*pseudoword' \
-    -gltLabel 5 word_vs_pseudoword_auditory -gltCode 5 'modality : 1*auditory lexicon : 1*word -1*pseudoword' \
-    -gltLabel 6 word_vs_pseudoword_visual -gltCode 6 'modality : 1*visual lexicon : 1*word -1*pseudoword' \
-    -dataTable @$ftab
+  fmvm="$tdir/stats.group_${task}_${model}_MVM"
+  if [ ! -f "${fmvm}+tlrc.HEAD" ];then
+    echo -e "Do MVM for the $task with $model. "
+    3dMVM -prefix $fmvm \
+      -jobs $njob \
+      -bsVars 1 \
+      -wsVars "modality*lexicon" \
+      -SS_type 3 \
+      -num_glt 6 \
+      -gltLabel 1 auditory_vs_visual -gltCode 1 'modality : 1*auditory -1*visual' \
+      -gltLabel 2 auditory_vs_visual_word -gltCode 2 'modality : 1*auditory -1*visual lexicon : 1*word' \
+      -gltLabel 3 auditory_vs_visual_pseudoword -gltCode 3 'modality : 1*auditory -1*visual lexicon : 1*pseudoword' \
+      -gltLabel 4 word_vs_pseudoword -gltCode 4 'lexicon : 1*word -1*pseudoword' \
+      -gltLabel 5 word_vs_pseudoword_auditory -gltCode 5 'modality : 1*auditory lexicon : 1*word -1*pseudoword' \
+      -gltLabel 6 word_vs_pseudoword_visual -gltCode 6 'modality : 1*visual lexicon : 1*word -1*pseudoword' \
+      -dataTable @$ftab
+  fi
+done
+## ---------------------------
+
+## extract beta coefficients
+for subj in ${subjects[@]};do
+  echo -e "Extract beta maps for the $task for $subj. "
+  wdir="$adir/$subj/$task"
+  # specify stat files
+  for model in ${models[@]};do
+    oglm="${subj}_${task}_${model}"
+    stat="$wdir/$oglm/stats.${subj}_${task}+tlrc.HEAD"
+    # extract coef maps for group analysis
+    i=0
+    for ilab in ${flab[@]};do
+      coef="$wdir/$oglm/stats.beta_${oglm}_${ilab}.nii.gz"
+      if [ ! -f $coef ];then
+        3dbucket -fbuc -prefix $coef "${stat}[${eidx[i]}]"
+      fi
+      let i+=1
+    done
+  done
+done
+## ---------------------------
+
+## Extract PSC for ROI analysis
+model='GLM.wBIM.wPSC.wNR24a'
+fpsc="$adir/group_${task}_${model}_PSC.csv"
+echo "participant_id,ROI_label,condition,PSC" >> $fpsc
+rads=(4 5 6 7 8)  # radii used for individual left-vOT ROIs
+for subj in ${subjects[@]};do
+  echo -e "Extract beta values (PSC) with ROIs for the $task for subject $subj."
+  wdir="$adir/$subj/$task"
+  oglm="${subj}_${task}_${model}"
+  # specify PSC beta files
+  for ilab in ${flab[@]};do
+    coef="$wdir/$oglm/stats.beta_${oglm}_${ilab}.nii.gz"
+    # extract PSC data for group ROIs
+    for iroi in ${rois[@]};do
+      froi="$kdir/group/group_${spac}_mask-${iroi}.nii.gz"
+      psc=$(3dBrickStat -mean -mask $froi $coef)
+      echo -e "$subj,$iroi,$ilab,$psc" >> $fpsc
+    done
+    # extract PSC beta for individual ROIs
+    for srad in ${rads[@]};do
+      froi="$kdir/$subj/${subj}_${spac}_mask-ilvOT-sph${srad}mm.nii.gz"
+      psc=$(3dBrickStat -mean -mask $froi $coef)
+      echo -e "$subj,ilvOT-sph${srad}mm,$ilab,$psc" >> $fpsc
+    done
+  done
 done
 ## ---------------------------
 
