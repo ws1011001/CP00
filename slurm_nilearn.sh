ddb5eb99ce42223e772bb033a5615c4ed8e9fdf4
Shuai Wang <ws1011001@gmail.com>
2023-04-14 15:10:26 +0200

    Cached.

--- slurm_nilearn.sh
+++ slurm_nilearn.sh
@@ -18,7 +18,8 @@ scripts_ni[0]='ps19_MVPA_classifier_selection_nilearn.py'
 scripts_ni[1]='ps20_MVPA_AudioVisAssos1word_ROI_classification_nilearn.py'
 scripts_ni[2]='ps21_MVPA_AudioVisAssos1word_searchlight_classification_nilearn.py'
 scripts_ni[3]='ps30_gMVPA_classifier_selection_nilearn.py'
-script_id=3
+scripts_ni[4]='ps31_gMVPA_AudioVisAssos1word_ROI_classification_nilearn.py'
+script_id=4
 
 # run nilearn script
 script_run=${scripts_ni[$script_id]}
