8f8c592daa15056c460c6b8319ba06ae531e713c
Shuai Wang <ws1011001@gmail.com>
2023-04-26 10:34:08 +0200

    Added flags to ignore GOFORIT errors.

--- ps12_GLM_AudioVisAssos1word_wBIM_wPSC_wNR24a_afni.sh
+++ ps12_GLM_AudioVisAssos1word_wBIM_wPSC_wNR24a_afni.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 ## ---------------------------
 ## [script name] ps12_GLM_AudioVisAssos1word_wBIM_wPSC_wNR24a_afni.sh
-##
 ## SCRIPT to ...
 ##
 ## By Shuai Wang, [date] 2020-10-20
-##
 ## ---------------------------
 ## Notes:
-##   
-##
 ## ---------------------------
 
-## set environment (packages, functions, working path etc.)
-# platform
+## Set environment (packages, functions, working path etc.)
+# Platform
 platform='mesoc'
 case "$platform" in
-  mesoc)
-    mdir='/CP00'                       # the project Main folder @mesocentre
-    export PATH="$mdir/nitools:$PATH"  # setup tools if @mesocentre
-    njob=16
-    ;;
-  totti)
-    mdir='/data/mesocentre/data/agora/CP00'  # the project Main folder @totti
-    njob=4
-    ;;
-  *)
-    echo -e "Please input a valid platform!"
-    exit 1
+	mesoc)
+		mdir='/CP00'                       # the project Main folder @mesocentre
+  	  	export PATH="$mdir/nitools:$PATH"  # setup tools if @mesocentre
+  	  	njob=4
+  	  	;;
+  	local)
+		mdir='/data/mesocentre/data/agora/CP00'  # the project Main folder @totti
+  	  	njob=4
+  	  	;;
+  	*)
+		echo -e "Please input a valid platform!"
+  	  	exit 1
 esac
 # setup path
 ddir="$mdir/AudioVisAsso"                # experiment Data folder (BIDS put into fMRIPrep)
@@ -50,53 +46,55 @@ hmth=0.5
 
 ## run GLM for each subject
 for subj in ${subjects[@]};do
-  echo -e "run GLM and statistical contrasts for $task for subject : $subj ......"
-  wdir="$adir/$subj/$task"                       # the Working folder
-  oglm="${subj}_${task}_GLM.wBIM.wPSC.w${deno}"  # the token for the Output GLM
-  # prepare data for GLM
-  3dDATAfMRIPrepToAFNI -fmriprep $ddir -subj $subj -task $task -nrun $nrun -runtag 'run-0' -deno $deno -spac $spac -cens $hmth -apqc $wdir/$oglm -bdmask $nrun
-  
-  # generate AFNI script
-  afni_proc.py -subj_id ${subj}_${task} \
-    -script $wdir/${oglm}.tcsh \
-    -out_dir $wdir/$oglm \
-    -copy_anat $adir/$subj/${subj}_${spac}_${anat}.nii.gz \
-    -anat_has_skull no \
-    -dsets $wdir/${subj}_${task}_run-*_${spac}_${bold}.nii.gz \
-    -blocks blur mask scale regress \
-    -blur_size $fwhm \
-    -blur_in_mask yes \
-    -blur_opts_BIM -mask $wdir/${subj}_${task}_${spac}_desc-brain_mask.nii.gz \
-    -mask_apply anat \
-    -regress_polort 2 \
-    -regress_local_times \
-    -regress_stim_times $wdir/stimuli/${subj}_${task}_events-cond*.txt \
-    -regress_stim_labels WA_cor WA_inc WV_cor WV_inc PA_cor PA_inc PV_cor PV_inc \
-    -regress_basis GAM \
-    -regress_motion_file $wdir/confounds/${subj}_${task}_${hmpv}.1D \
-    -regress_motion_per_run \
-    -regress_censor_extern $wdir/confounds/${subj}_${task}_${cenv}.1D \
-    -regress_opts_3dD \
-      -ortvec $wdir/confounds/${subj}_${task}_run-01_${ortv}_all.1D nuisance_regressors_run1 \
-      -ortvec $wdir/confounds/${subj}_${task}_run-02_${ortv}_all.1D nuisance_regressors_run2 \
-      -ortvec $wdir/confounds/${subj}_${task}_run-03_${ortv}_all.1D nuisance_regressors_run3 \
-      -ortvec $wdir/confounds/${subj}_${task}_run-04_${ortv}_all.1D nuisance_regressors_run4 \
-      -ortvec $wdir/confounds/${subj}_${task}_run-05_${ortv}_all.1D nuisance_regressors_run5 \
-      -cbucket coefs -xsave \
-    -jobs $njob \
-    -html_review_style pythonic
-  
-  # modify the script nd run it
-  sed -e '39 {s/^/#/}' -e '46 {s/^/#/}' $wdir/${oglm}.tcsh > $wdir/${oglm}_exec.tcsh  # comment the line 39 to ignore the exist of out_dir
-  tcsh -xef $wdir/${oglm}_exec.tcsh 2>&1 | tee $wdir/output_${oglm}_exec.tcsh         # execute the AFNI script
-  
-  # backup confounds (.1D files) for the present GLM
-  tar -cvzf $wdir/confounds/${oglm}.1D.tar.gz $wdir/confounds/*.1D
-  rm -r $wdir/confounds/*.1D
+	echo -e "run GLM and statistical contrasts for $task for subject : $subj ......"
+  	wdir="$adir/$subj/$task"                       # the Working folder
+  	oglm="${subj}_${task}_GLM.wBIM.wPSC.w${deno}"  # the token for the Output GLM
+  	# prepare data for GLM
+  	3dDATAfMRIPrepToAFNI -fmriprep $ddir -subj $subj -task $task -nrun $nrun -runtag 'run-0' -deno $deno -spac $spac -cens $hmth -apqc $wdir/$oglm -bdmask $nrun
+  	
+  	# generate AFNI script
+  	afni_proc.py -subj_id ${subj}_${task} \
+		-script $wdir/${oglm}.tcsh \
+  	  	-out_dir $wdir/$oglm \
+  	  	-copy_anat $adir/$subj/${subj}_${spac}_${anat}.nii.gz \
+  	  	-anat_has_skull no \
+  	  	-dsets $wdir/${subj}_${task}_run-*_${spac}_${bold}.nii.gz \
+  	  	-blocks blur mask scale regress \
+  	  	-blur_size $fwhm \
+  	  	-blur_in_mask yes \
+  	  	-blur_opts_BIM -mask $wdir/${subj}_${task}_${spac}_desc-brain_mask.nii.gz \
+  	  	-mask_apply anat \
+  	  	-regress_polort 2 \
+  	  	-regress_local_times \
+  	  	-regress_stim_times $wdir/stimuli/${subj}_${task}_events-cond*.txt \
+  	  	-regress_stim_labels WA_cor WA_inc WV_cor WV_inc PA_cor PA_inc PV_cor PV_inc \
+  	  	-regress_basis GAM \
+  	  	-regress_motion_file $wdir/confounds/${subj}_${task}_${hmpv}.1D \
+  	  	-regress_motion_per_run \
+  	  	-regress_censor_extern $wdir/confounds/${subj}_${task}_${cenv}.1D \
+  	  	-regress_opts_3dD \
+			-ortvec $wdir/confounds/${subj}_${task}_run-01_${ortv}_all.1D nuisance_regressors_run1 \
+  	  	  	-ortvec $wdir/confounds/${subj}_${task}_run-02_${ortv}_all.1D nuisance_regressors_run2 \
+  	  	  	-ortvec $wdir/confounds/${subj}_${task}_run-03_${ortv}_all.1D nuisance_regressors_run3 \
+  	  	  	-ortvec $wdir/confounds/${subj}_${task}_run-04_${ortv}_all.1D nuisance_regressors_run4 \
+  	  	  	-ortvec $wdir/confounds/${subj}_${task}_run-05_${ortv}_all.1D nuisance_regressors_run5 \
+			-allzero_OK \
+			-GOFORIT 99 \
+  	  	  	-cbucket coefs -xsave \
+  	  	-jobs $njob \
+  	  	-html_review_style pythonic
+  	
+  	# modify the script nd run it
+  	sed -e '39 {s/^/#/}' -e '46 {s/^/#/}' $wdir/${oglm}.tcsh > $wdir/${oglm}_exec.tcsh  # comment the line 39 to ignore the exist of out_dir
+  	tcsh -xef $wdir/${oglm}_exec.tcsh 2>&1 | tee $wdir/output_${oglm}_exec.tcsh         # execute the AFNI script
+  	
+  	# backup confounds (.1D files) for the present GLM
+  	tar -cvzf $wdir/confounds/${oglm}.1D.tar.gz $wdir/confounds/*.1D
+  	rm -r $wdir/confounds/*.1D
 done
 ## ---------------------------
 
 ## summarize data quality metrics
 gen_ss_review_table.py -write_table $adir/review_QC_${task}_GLM.wBIM.wPSC.w${deno}.tsv \
-  -infiles $adir/sub-*/$task/sub-*_${task}_GLM.wBIM.wPSC.w${deno}/out.ss_review.sub-*_${task}.txt -overwrite
+	-infiles $adir/sub-*/$task/sub-*_${task}_GLM.wBIM.wPSC.w${deno}/out.ss_review.sub-*_${task}.txt -overwrite
 ## ---------------------------
