5e28bade8b49049e9129130ddb07bedb7ad08928
Shuai Wang <ws1011001@gmail.com>
2023-12-02 11:41:59 +0100

    Clean up.

--- ps27_RSE_AudioVisAssos2words_extract_PSC_and_TENT_afni.sh
+++ ps27_RSE_AudioVisAssos2words_extract_PSC_and_TENT_afni.sh
@@ -8,7 +8,7 @@
 
 ## Set environment (packages, functions, working path etc.)
 # Platform
-platform='local'
+platform='mesoc'
 case "$platform" in
 	mesoc)
 		dir_main='/CP00'                       # the project Main folder @mesocentre
