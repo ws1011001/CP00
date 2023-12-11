4c9e6b407319d00742e6d5b79973534d4718b57f
Shuai Wang <ws1011001@gmail.com>
2023-12-11 15:32:26 +0100

    Added code to do T-tests with FWE estimation.

--- ps22_STAT_AudioVisAssos1word_searchlight_ACC_afni.sh
+++ ps22_STAT_AudioVisAssos1word_searchlight_ACC_afni.sh
@@ -32,8 +32,8 @@ dir_resl="$dir_mvpa/group"                     # group results folder
 readarray subjects < $dir_main/CP00_subjects.txt
 task='task-AudioVisAssos1word'                    # task name
 spac='space-MNI152NLin2009cAsym'                  # anatomical template that used for preprocessing by fMRIPrep
-#mask="$dir_mask/group/group_${spac}_mask-gm0.2_res-${task}.nii.gz"
-mask="$dir_mask/group/group_${spac}_mask-gm-left-ventral-pathway.nii.gz"
+mask="$dir_mask/group/group_${spac}_mask-gm0.2_res-${task}.nii.gz"
+#mask="$dir_mask/group/group_${spac}_mask-gm-left-ventral-pathway.nii.gz"
 clfs=("LDA" "GNB" "SVClin" "SVCrbf")              # classifier tokens
 base_acc=0.5                                      # the chance level i.e. 50%
 ## ---------------------------
@@ -78,11 +78,11 @@ for clf in ${clfs[@]};do
 			3dcalc -a $f_acc -expr "a-$base_acc" -prefix $f_abv  # calculate above-chance ACC
 		fi
   	  	# T-test on one sample againest the chance level with FWE estimation
-		f_test="$dir_resl/stats.lVP.group_gMVPA-${clf}_LOSOCV_ACC-above-chance-${imod}_mask-gm0.2_res-${task}"
-		f_resid="$dir_resl/stats.lVP.group.resid_gMVPA-${clf}_LOSOCV_ACC-above-chance-${imod}_mask-gm0.2_res-${task}+tlrc"
-		f_acf="$dir_resl/stats.lVP.group.ACF_gMVPA-${clf}_LOSOCV_ACC-above-chance-${imod}_mask-gm0.2_res-${task}"
-		f_sim="$dir_resl/stats.lVP.group.ACFc_gMVPA-${clf}_LOSOCV_ACC-above-chance-${imod}_mask-gm0.2_res-${task}"
-		f_fwe="$dir_resl/stats.lVP.group.FWE_gMVPA-${clf}_LOSOCV_ACC-above-chance-${imod}_mask-gm0.2_res-${task}"
+		f_test="$dir_resl/stats.group_gMVPA-${clf}_LOSOCV_ACC-above-chance-${imod}_mask-gm0.2_res-${task}"
+		f_resid="$dir_resl/stats.group.resid_gMVPA-${clf}_LOSOCV_ACC-above-chance-${imod}_mask-gm0.2_res-${task}+tlrc"
+		f_acf="$dir_resl/stats.group.ACF_gMVPA-${clf}_LOSOCV_ACC-above-chance-${imod}_mask-gm0.2_res-${task}"
+		f_sim="$dir_resl/stats.group.ACFc_gMVPA-${clf}_LOSOCV_ACC-above-chance-${imod}_mask-gm0.2_res-${task}"
+		f_fwe="$dir_resl/stats.group.FWE_gMVPA-${clf}_LOSOCV_ACC-above-chance-${imod}_mask-gm0.2_res-${task}"
 		if [ ! -f "${f_acf}.1D" ];then
 			echo -e "Perform one-sample T-test for the $imod decoding."
 			# Perform paired T-test
